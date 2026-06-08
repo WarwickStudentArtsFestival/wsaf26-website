83fd2405a5efe46550bb1f2cd807dd39870cd596
Adam Skrzymowski <35405850+adamskrz@users.noreply.github.com>
2026-06-08 22:07:53 +0100

    Add helfertool link

--- config/main-config.ts
+++ config/main-config.ts
@@ -33,7 +33,7 @@ const mainConfig: MainConfig = {
     submitUrl: 'https://submit.wsaf.org.uk/2026/cfp',
   },
   crew: {
-    // signupUrl: 'https://helfertool.wsaf.org.uk/wsaf2025/',
+    signupUrl: 'https://helfertool.wsaf.org.uk/wsaf-2026/',
     signupUrl: 'https://discord.gg/TuFwJX4GKM',
   },
   feedback: {
